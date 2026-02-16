d8b3d71192a58824a8788744f1e3a842a52f760d
vadim-ggt <koval.cap.stop.j@gmail.com>
2026-02-16 19:04:10 +0300

    init commit

---
+++ src/main/java/com/crypto_flow/crypto_flow/CryptoFlowApplication.java
@@ -0,0 +1,13 @@
+package com.crypto_flow.crypto_flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crypto_flow/crypto_flow/CryptoFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crypto_flow.crypto_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
